b56ec24a49741135f4fbbc39fd59fde2c91014fe
Marco Nardes <marconardes@gmail.com>
2017-10-10 08:14:14 -0300

    Update

--- FindMetrics/noob.py
+++ FindMetrics/noob.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 import csv
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -30,9 +29,9 @@ def plotLineGraf(hashMap,labelx,labely):
     for key, value in hashMap.items():
         x = key.split('/')
         if "Nardes" in key:
-            plt.plot(value, label="Com Metadata", color='Red')
+            plt.plot(value, label="With Metadata", color='Red')
         elif "Guerra" in key:
-            plt.plot(value, label="Sem Metadata", color='Blue')
+            plt.plot(value, label="Without Metadata", color='Blue')
         elif "exp1groupA" in key:
             plt.plot(value, color='Blue')
         elif "exp1groupB" in key:
@@ -44,62 +43,20 @@ def plotLineGraf(hashMap,labelx,labely):
     plt.show()
 
 
-def calcularMedia(hashElement):
-    print"nao implementado"
-    i = 0
-    list = []
-    
-    for key, value in hashElement.items():
-        list.append(value)
-        i=i+1
-    
-    arr = list[0]
-    
-    for x in range(1, len(list)):
-        for z in range(0, len(list[x])):
-            arr[z]= arr[z] +list[x][z]
+def generateIncrement(hashMap):
+    dic ={}
+    for key, value in hashMap.items():
+        obj = list()
+        tamanho = len(value)
+        for x in range(0, tamanho):
+            if x>0:
+                obj.append(value[x]-value[x-1])
+            else:
+                obj.append(value[x])
+       
+        dic.setdefault(key, obj)
         
-    
-    for x in range(0, len(arr)):
-        arr[x] = arr[x]/i
-    return arr
-    
-
-def calcularDp(hashElement):
-    print"nao implementado"
-    
-    a = []
-    
-    #a =[[1,2,3],[1,2,3]]
-    
-    c  = 0
-    
-    for key, value in hashElement.items():
-        print value
-        a[c].append(value)        
-        c = c+1
-    
-    aInv = []
-    
-    print len(aInv)
-    
-    print len(a)
-    
-    for x in range(0, len(aInv)):
-        print "x    "+ str(x)    
-        value = []
-        for y in range(0, len(a)):
-            print "YYYYY   "+str(y)
-            b = a[y]
-            
-            #print(b)
-            value.append(b[x])
-        print value
-        aInv[x].append(value)
-    
-    print aInv
-    return aInv
-
+    return dic
     
 
 def plotBarGraf(hash,figname,a, b,c):
@@ -108,39 +65,69 @@ def plotBarGraf(hash,figname,a, b,c):
         for x in range(0, len(value)):
             y = x + 1
             if "Nardes" in key:
-                pdd.loc[len(pdd)] = ['Com Metadata', "Task " + str(y), value[x]]
+                pdd.loc[len(pdd)] = ['With Metadata', "Task " + str(y), value[x]]
             elif "Guerra" in key:
-                pdd.loc[len(pdd)] = ['Sem Metadata', "Task " + str(y), value[x]]
+                pdd.loc[len(pdd)] = ['Without Metadata', "Task " + str(y), value[x]]
             elif "exp1groupA" in key:
-                pdd.loc[len(pdd)] = ['Sem Metadata', "Task " + str(y), value[x]]
+                pdd.loc[len(pdd)] = ['Without Metadata', "Task " + str(y), value[x]]
             elif "exp1groupB" in key:
-                pdd.loc[len(pdd)] = ['Com Metadata', "Task " + str(y), value[x]]
+                pdd.loc[len(pdd)] = ['With Metadata', "Task " + str(y), value[x]]
     
     sns.set_style("whitegrid")
 
     flatui = ["#99bbff", "#ff8080"]
     sns.set_palette(flatui)
-    #sns.palplot(sns.color_palette())    
     ax = sns.boxplot(x=c, y="TASK", hue="TIPO", data=pdd)
     ax.get_figure().savefig(figname)
     ax.get_figure().clf()
-
     
-    
+def xdivy(hash1,hash2):
+    f={}
+    z = len(hash1)
+    print z
+    return f
     
 
 listCsv = list()
-hashMap={}
+
+loc={}
+
 ReadCsv(listCsv)
 tamanho = len(listCsv)
-getFieldList(listCsv, list, hashMap, tamanho, 2)
-plotLineGraf(hashMap,"Commits","Loc")
-
-hashMap2={}
-getFieldList(listCsv, list, hashMap2, tamanho, 3)
-plotLineGraf(hashMap2,"Commits","WMC")
+getFieldList(listCsv, list, loc, tamanho, 2)
+plotLineGraf(loc,"Commits","Loc")
+plotBarGraf(loc,"loc.png","TIPO","TASK","LOC")
 
 
-plotBarGraf(hashMap,"loc.png","TIPO","TASK","LOC")
 
-plotBarGraf(hashMap2,"wmc.png","TIPO","TASK","WMC")
+wmc={}
+getFieldList(listCsv, list, wmc, tamanho, 3)
+plotLineGraf(wmc,"Commits","WMC")
+plotBarGraf(wmc,"wmc.png","TIPO","TASK","WMC")
+
+cbo={}
+getFieldList(listCsv, list, cbo, tamanho, 4)
+plotLineGraf(wmc,"Commits","CBO")
+plotBarGraf(wmc,"cbo.png","TIPO","TASK","CBO")
+
+lcom={}
+getFieldList(listCsv, list, lcom, tamanho, 5)
+plotLineGraf(wmc,"Commits","LCOM")
+plotBarGraf(wmc,"lcom.png","TIPO","TASK","LCOM")
+
+nom={}
+getFieldList(listCsv, list, nom, tamanho, 5)
+plotLineGraf(wmc,"Commits","NOM")
+plotBarGraf(wmc,"nom.png","TIPO","TASK","NOM")
+
+nof={}
+getFieldList(listCsv, list, nom, tamanho, 5)
+plotLineGraf(wmc,"Commits","NOF")
+plotBarGraf(wmc,"nof.png","TIPO","TASK","NOF")
+
+zzz = xdivy(wmc,nom)
+
+
+incremento = generateIncrement(loc)
+
+plotBarGraf(incremento,"incremento.png","TIPO","TASK","INCREMENTO")
